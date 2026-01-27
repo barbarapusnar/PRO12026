95141c1b1c7b436d940f8e675631b6dca1bba649
Barbara Pušnar <barbara.pusnar@scng.si>
2026-01-27 12:40:02 +0100

    27.1

--- Kalkulatorv0.2/Kalkulatorv0.2/Program.cs
+++ Kalkulatorv0.2/Kalkulatorv0.2/Program.cs
@@ -1,46 +1,58 @@
-﻿namespace Kalkulatorv0._2
+﻿
+namespace Kalkulatorv0._2
 {
+    using System;
+    using System.Windows.Forms;
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Vnesi oddelek študenta 1-ang,2-mat,3-nar");
-            int oddelek = int.Parse(Console.ReadLine());
-            Odseki o = (Odseki)oddelek;
-            int delni1 = 0;
-            int delni2 = 0;
-            int seminar = 0;
-            int ustna = 0;
-            double končna = 0;
-            string črka = "";
-            //glede na oddelek študenta
-            switch (o)
+            string šeKakšenŠtudent = "";
+            do
             {
-                case Odseki.Angleščina: 
-                    //napiši kodo
-                    break;
-                case Odseki.Matematika: //študent mat
-                    Console.WriteLine("Vnesi oceno delnega izpita 1");
-                    delni1 = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Vnesi oceno delnega izpita 2");
-                    delni2 = int.Parse(Console.ReadLine());
-                    končna = 0.5 * delni1 + 0.5 * delni2;
-                    if (končna >= 90)
-                        črka = "A";
-                    else if (končna >= 83)
-                        črka = "B";
-                    else if (končna >= 76)
-                        črka = "C";
-                    else if (končna >= 65)
-                        črka = "D";
-                    else
-                        črka = "F";
-                    Console.WriteLine("Končna ocena = "+končna);
-                    Console.WriteLine("Končna črka  = "+črka);
+                Console.WriteLine("Vnesi oddelek študenta 1-ang,2-mat,3-nar");
+                int oddelek = int.Parse(Console.ReadLine());
+                Odseki o = (Odseki)oddelek;
+                int delni1 = 0;
+                int delni2 = 0;
+                int seminar = 0;
+                int ustna = 0;
+                double končna = 0;
+                string črka = "";
+                //glede na oddelek študenta
+                switch (o)
+                {
+                    case Odseki.Angleščina:
+                        //napiši kodo
                         break;
-                case Odseki.Naravoslovje://študent naravoslovja
-                    break;
-            }
-        }
+                    case Odseki.Matematika: //študent mat
+                        Console.WriteLine("Vnesi oceno delnega izpita 1");
+                        delni1 = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Vnesi oceno delnega izpita 2");
+                        delni2 = int.Parse(Console.ReadLine());
+                        končna = 0.5 * delni1 + 0.5 * delni2;
+                        if (končna >= 90)
+                            črka = "A";
+                        else if (končna >= 83)
+                            črka = "B";
+                        else if (končna >= 76)
+                            črka = "C";
+                        else if (končna >= 65)
+                            črka = "D";
+                        else
+                            črka = "F";
+                        //Console.WriteLine("Končna ocena = " + končna);
+                        //Console.WriteLine("Končna črka  = " + črka);
+                        MessageBox.Show("Končna ocena " + končna +
+                            Environment.NewLine + "Končna črka  = " + črka,"VSŠ",
+                            MessageBoxButtons.OK,MessageBoxIcon.Information);
+                        break;
+                    case Odseki.Naravoslovje://študent naravoslovja
+                        break;
+                }//konec switch
+                Console.WriteLine("Še kakšen študent? DA/NE");
+                šeKakšenŠtudent = Console.ReadLine().ToUpper();
+            } while (šeKakšenŠtudent == "DA");
+     }
     }
 }
---
+++ Ruleta/Ruleta/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace Ruleta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //            1.uporabnik naj vnese začetni denarni vložek, shrani ga v spremenljivko stanje
+            Console.WriteLine("Koliko denarja si prinesel za zapraviti?");
+            int stanje = int.Parse(Console.ReadLine());
+            //2.uporabnik naj vnese začetno višino stave, shrani ga v spremenljivko stava
+            Console.WriteLine("Koliko je začetna stava?");
+            int stava = int.Parse(Console.ReadLine());
+            //3.V spremenljivkah stanje in stava bom imela trenutno stanje in trenutno stavo, zapomniti pa si
+            //moram tudi znesek s katerim sem začela igrati in začetno stavo  deklarirajte spremenljivki
+            //začStanje(ki je na začetku enaka stanje) in začStava(= stava)
+            int začStanje = stanje;
+            int začStava = stava;
+            //4.Ker bi radi imeli tudi največje oziroma najmanjše stanje, ki smo ga med meti imeli, si na tem
+            //mestu deklarirajmo še spremenljivki min in max.
+            int min = 100000;
+            int max = -100000;
+            Random r = new Random();
+            //5. 10 krat ponovimo
+            for(int k=1;k<=1000;k++)
+            {
+                //a.vrzi kroglico(naključno število med 0 in 36)
+                int število = r.Next(37);
+                //b.če je število sodo in različno od 0
+                if (število % 2 == 0 && število != 0)
+                {
+                    //i.stavo smo dobili  povečaj stanje za stavo, stava = začStava, barva = rdeča
+                    stanje += stava;
+                    stava = začStava;
+                    Console.WriteLine(k + ". " + " rdeča - zmaga " + stanje);
+                }
+                else
+                {
+                    //c.sicer
+                    //i.stavo smo izgubilizmanjšaj stanje za stavo, stava = 2 * stava, barva = črna
+                    stanje -= stava;
+                    stava = 2*stava;
+                    Console.WriteLine(k + ". " + " črna - izguba " + stanje);
+                }
+                if (stanje < min)
+                    min = stanje;
+                if (stanje > max)
+                    max = stanje;
+                //e.preveri ali je stanje manjše od min
+                //f.preveri ali je stanje večje od max
+            }
+            Console.WriteLine("Število metov 10");
+            Console.WriteLine("Na začetku "+začStanje);
+            Console.WriteLine("Na koncu "+stanje);
+            Console.WriteLine("Min      "+min);
+            Console.WriteLine("Max      "+max);
+            Console.WriteLine("Dobiček  "+(stanje-začStanje));
+            //6.Na koncu izpiši:
+            //a.število metov
+            //b.začetno stanje
+            //c.končno stanje
+            //d.najvišje stanje
+            //e.najnižje stanje
+            //f.dobiček ali izgubo(končno stanje – začetno stanje)
+        }
+    }
+}
